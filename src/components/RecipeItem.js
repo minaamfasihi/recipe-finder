2a70c86b545db63416e805a62759a5fbe100dcef
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-12 01:10:00 +0500

    Added RecipeItem

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 class RecipeItem extends Component {
     render() {
+        let { recipe } = this.props;
         return(
-            <h4>
-                {this.props.recipe.title}
-            </h4>
+            <div className="recipe-item">
+                <div className="recipe-text">
+                    <a href={recipe.href}>
+                        <h4>{recipe.title}</h4>
+                    </a>
+                    <p>{recipe.ingredients}</p>
+                </div>
+                <img 
+                    src={recipe.thumbnail} 
+                    alt={recipe.title} 
+                    className="recipe-img" 
+                />
+            </div>
         )
     }
 }
