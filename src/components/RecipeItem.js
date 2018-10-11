43778d73a10a1fd913243c5837e02fc195b7f76c
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-12 02:38:19 +0500

    Added react router and other stuff

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { favoriteRecipe } from '../actions';
 
 class RecipeItem extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            favorited: false
+        }
+    }
+
+    favorite(recipe) {
+        this.props.favoriteRecipe(recipe);
+        this.setState({ favorited: true });
+    }
+
     render() {
         let { recipe } = this.props;
         return(
             <div className="recipe-item">
+                {
+                    this.props.favoriteButton ?
+                        this.state.favorited ?
+                            <div className="star">&#9733;</div>
+                        :
+                        <div
+                            className="star"
+                            onClick={() => this.favorite(recipe)}
+                        >
+                            &#9734;
+                        </div>
+                    :
+                    <div></div>
+                }
                 <div className="recipe-text">
                     <a href={recipe.href}>
                         <h4>{recipe.title}</h4>
@@ -21,4 +50,4 @@ class RecipeItem extends Component {
     }
 }
 
-export default RecipeItem;
+export default connect(null, { favoriteRecipe })(RecipeItem);
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RecipeItem from './RecipeItem';
+import { Link } from 'react-router-dom';
 
 class RecipeList extends Component {
     render() {
         return(
             <div>
                 {
+                    this.props.favoriteRecipes.length > 0 ?
+                    <h4><Link className="link" to="/favorites">Favorites</Link></h4>
+                    :
+                    <div></div>
+                }
+                {
                     this.props.recipes.map((recipe, index) => {
                         return(
-                            <RecipeItem key={index} recipe={recipe} />
+                            <RecipeItem 
+                                key={index} 
+                                recipe={recipe}
+                                favoriteButton={true} 
+                            />
                         )
                     })
                 }                
